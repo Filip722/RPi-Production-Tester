bd828c58b808a01f318effc8883f0eb7eb26dc08
Filip Michalski <filip.michalski92@gmail.com>
2019-08-26 02:01:57 +0200

    cleaned up the code from leftovers and added comments

--- main.py
+++ main.py
@@ -9,23 +9,19 @@ from PyQt5.QtCore import QThread, pyqtSignal
 from PyQt5.QtWidgets import QDialog, QApplication
 from random import randint
 
-DEBUG = 1  # set to get data from randint instead of real data from ADC ADS1115
+DEBUG = True  # set to get data from randint instead of real data from ADC ADS1115
 
-ConfigDirectory = os.path.join(os.getcwd(), "Configs")
-remoteConfigDirectory = "C:/configs"
+ConfigDirectory = os.path.join(os.getcwd(), "Configs")  # Path to configs
+remoteConfigDirectory = '//192.168.1.2/home/RpiTester/Configs'  # remote path to configs
+refreshRate = 100  # refresh rate in milliseconds
 
-GAIN = 1  # 4.096V
-
-# PATH = "/home/pi/Desktop/Moduly/"
-PATH = "C:/"
-
-CurrentBase = 69
-CurrentTolerance = 15
-VoltageBase = 24
-VoltageTolerance = 15
+CurrentBase = 1
+CurrentTolerance = 0
+VoltageBase = 1
+VoltageTolerance = 0
 AdditionalInfo = ""
 
-adc_divider = [2, 0.1]
+adc_divider = [1, 1]  # set this to calibrate according to dividers / shunts
 voltageMeasured = [0, 0]
 
 
@@ -47,10 +43,9 @@ class GetADCThread(QThread):
     def run(self):
         self.running = True
         while self.running:
-            QThread.msleep(100)
-            # QThread.sleep(2)
+            QThread.msleep(refreshRate)
 
-            if DEBUG is 0:
+            if DEBUG is False:
                 for i in range(0, 1):
                     adc = Adafruit_ADS1x15.ADS1115().read_adc(i, gain=1)
                     voltageMeasured[i] = adc / 65535 * 4.096 * adc_divider[i]
@@ -61,7 +56,6 @@ class GetADCThread(QThread):
             self.voltageSignal.emit(f'{format(voltageMeasured[0], ".2f")}')
             self.currentSignal.emit(f'{format(voltageMeasured[1], ".2f")}')
 
-            #print(f'voltage0={voltageMeasured[0]}, voltage[1]={voltageMeasured[1]}')
             minCurrent = CurrentBase / 100 * (100 - CurrentTolerance)
             maxCurrent = CurrentBase / 100 * (100 + CurrentTolerance)
             minVoltage = VoltageBase / 100 * (100 - VoltageTolerance)
@@ -85,6 +79,7 @@ class GetADCThread(QThread):
                 self.pushButtonALL.emit("border-radius:50px; background-color: rgb(0,255,0);")
             else:
                 self.pushButtonALL.emit("border-radius:50px; background-color: rgb(255,0,0);")
+
             VoltageDeviation = (1 - (voltageMeasured[0] / VoltageBase)) * 100
             VoltageDeviation = f'{format(VoltageDeviation, ".2f")}'
             self.DeviationVoltageLabel.emit(f'{VoltageDeviation}%')
@@ -92,28 +87,6 @@ class GetADCThread(QThread):
             CurrentDeviation = (1 - (voltageMeasured[1] / CurrentBase)) * 100
             CurrentDeviation = f'{format(CurrentDeviation, ".2f")}'
             self.DeviationCurrentLabel.emit(f'{CurrentDeviation}%')
-
-
-'''
-def CheckInputVoltage():
-    minVoltage = InputVoltageBase / 100 * (100 - InputVoltageTolerance)
-    maxVoltage = InputVoltageBase / 100 * (100 + InputVoltageTolerance)
-
-    if voltageMeasured[0] > minVoltage and voltageMeasured[0] < maxVoltage:
-        return True
-    else:
-        return False
-'''
-
-
-def copytree(src, dst, symlinks=False, ignore=None):
-    for item in os.listdir(src):
-        s = os.path.join(src, item)
-        d = os.path.join(dst, item)
-        if os.path.isdir(s):
-            shutil.copytree(s, d, symlinks, ignore)
-        else:
-            shutil.copy2(s, d)
 
 
 class AppWindow(QDialog):
@@ -125,9 +98,9 @@ class AppWindow(QDialog):
         self.ui.setupUi(self)
         self.show()
         self.getBoards()
+
         self.ui.comboBoxDirs.currentIndexChanged.connect(self.getConfigs)
         self.ui.comboBoxConfigs.currentIndexChanged.connect(self.loadConfig)
-
         self.ui.pushButtonLoad.clicked.connect(self.copyConfigs)
 
         self.getADCThread = GetADCThread()
@@ -143,7 +116,6 @@ class AppWindow(QDialog):
         self.getADCThread.pushButtonALL.connect(self.ui.pushButtonALL.setStyleSheet)
 
         self.ui.pushButtonALL.setStyleSheet("border-radius:50px; background-color: rgb(255,0,0);")
-        # self.lalala()
 
         app.processEvents()
 
@@ -170,39 +142,24 @@ class AppWindow(QDialog):
             self.ui.labelSetVoltageTo.setText(f'{format(VoltageBase, ".2f")}')
 
     def copyConfigs(self):
-        shutil.rmtree(ConfigDirectory)
-        copytree(remoteConfigDirectory, ConfigDirectory)
+        shutil.rmtree(ConfigDirectory, ignore_errors=True)
+        for item in os.listdir(remoteConfigDirectory):
+            shutil.copytree(remoteConfigDirectory, ConfigDirectory)
         self.getBoards()
 
     def getConfigs(self):
         self.ui.comboBoxConfigs.clear()
-        self.ui.comboBoxConfigs.addItem("")
+        self.ui.comboBoxConfigs.addItem("")  # to make sure user selects a config!
         for file in os.listdir(os.path.join(ConfigDirectory, self.ui.comboBoxDirs.currentText())):
             if file.endswith(".config"):
                 self.ui.comboBoxConfigs.addItem(file[:-7])
 
     def getBoards(self):
         self.ui.comboBoxDirs.clear()
-        self.ui.comboBoxDirs.addItem("")
+        self.ui.comboBoxDirs.addItem("")  # to make sure user selects a board!
         for file in os.listdir(ConfigDirectory):
             if os.path.isdir(os.path.join(ConfigDirectory, file)):
                 self.ui.comboBoxDirs.addItem(file)
-
-    def lalala(self):
-        i = 0
-        while True:
-
-            app.processEvents()
-            time.sleep(0.1)
-            i = i + 1
-            # value = adc.read_adc(3, gain=1)/100
-            value = 150
-            value = int(value)
-            # print(value)
-            if value > 100 and value < 200:
-                self.ui.pushButtonALL.setStyleSheet("border-radius:50px; background-color: rgb(0, 255, 0);")
-            else:
-                self.ui.pushButtonALL.setStyleSheet("border-radius:50px; background-color: rgb(255, 0, 0);")
 
 
 app = QApplication(sys.argv)
